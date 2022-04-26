a432ac5331c96a0376fdc6a273fd7f138edf0ba4
najla_hamza <najla.hamza11@gmail.com>
2022-04-26 14:05:04 +0100

    f2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDT9BnC3HvTvVznzRWK0JyhhrFIiWqbQP8",
+    authDomain: "orsomni-2f317.firebaseapp.com",
+    projectId: "orsomni-2f317",
+    storageBucket: "orsomni-2f317.appspot.com",
+    messagingSenderId: "765668571448",
+    appId: "1:765668571448:web:e022f2707dbe84d4f9e9cd",
+    measurementId: "G-P8BYMY2Q3V"
+  },
 };
